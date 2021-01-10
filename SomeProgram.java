10eda18825e6218f96c5de4dd66092639c274b8b
Aleksandra Zajdel <zaleksandra@student.agh.edu.pl>
2021-01-10 15:28:32 +0100

    Merging managing satelites

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,12 +3,12 @@ public class SomeProgram {
       System.out.println("We are learning to use Git."); 
 	  System.out.println("We will be Git experts, soon.");
     }
-<<<<<<< HEAD
 	
 	public void manageTheRockets() {
-=======
+		// TODO implement
+	}
 	public void manageTheSatellite() {
->>>>>>> managing-satellite
+
     // TODO implement
 }
 }
